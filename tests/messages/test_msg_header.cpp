6b4d1d12cd73d862fa2aa62cc3701c5ad8010eea
James Bourne <james.a.bourne.85@gmail.com>
2024-11-27 15:25:41 +0000

    Refactoring the project structure, and adding initial unit tests for the msg header class

---
+++ tests/messages/test_msg_header.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "messages/MsgHeader.h"
+
+TEST(AddTest, msg_header_constructor)
+{
+    unsigned int id = 100;
+    unsigned int length = sizeof(MsgHeader);
+
+    MsgHeader header(id, length);
+
+    EXPECT_EQ(header.id, id);
+    EXPECT_EQ(header.length, length);
+}
+
+TEST(AddTest, msg_header_serialise_deserialise_test)
+{
+    unsigned int id = 100;
+    unsigned int length = sizeof(MsgHeader);
+
+    MsgHeader header(id, length);
+
+    std::vector<char> buffer;
+    header.serialize(buffer);
+    
+    EXPECT_EQ(buffer.size(), length);
+
+    MsgHeader empty_header(0, length);
+
+    empty_header.deserialize(buffer);
+
+    EXPECT_EQ(empty_header.id, id);
+    EXPECT_EQ(empty_header.length, length);    
+    EXPECT_EQ(empty_header.timestamp, header.timestamp);
+}
