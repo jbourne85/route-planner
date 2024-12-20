45daad63496d5e32650cffaf1b6a7931e7e48e60
James Bourne <james.a.bourne.85@gmail.com>
2024-12-20 12:59:14 +0000

    Adding messages namespace to the msg factory

--- src/messages/MsgFactory.cpp
+++ src/messages/MsgFactory.cpp
@@ -1,17 +1,22 @@
+#include <boost/asio.hpp>
+#include <iostream>
 #include "messages/MsgFactory.h"
 #include "messages/MsgHeader.h"
 #include "messages/MsgStatus.h"
 
-MsgHeader::pointer construct_message(const unsigned int id)
-{
+namespace messages {
+
+MsgHeader::MsgPointer construct_message(const unsigned int id) {
     switch(id)
     {
         case MSG_HEADER_ID:
-            return MsgHeader::pointer(new MsgHeader(MSG_HEADER_ID, sizeof(MsgHeader))); 
+            return MsgHeader::MsgPointer(new MsgHeader(MSG_HEADER_ID, sizeof(MsgHeader))); 
         case MSG_STATUS_REQUEST_ID:
-            return MsgHeader::pointer(new MsgStatusRequest()); 
+            return MsgHeader::MsgPointer(new MsgStatusRequest()); 
         case MSG_STATUS_RESPONSE_ID:
-            return MsgHeader::pointer(new MsgStatusResponse()); 
+            return MsgHeader::MsgPointer(new MsgStatusResponse()); 
     }
-    return MsgHeader::pointer(nullptr);
+    return MsgHeader::MsgPointer(nullptr);
+}
+
 }
