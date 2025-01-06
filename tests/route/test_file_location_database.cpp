84270871f865f261a143b9e7b1faddb442007c61
James Bourne <james.a.bourne.85@gmail.com>
2025-01-06 17:20:12 +0000

    Adding a test for the utlity GetLocationByName method, ensuring the lcoation cleanup is tested. Added breifs for the FileLocationDatabase tests

--- tests/route/test_file_location_database.cpp
+++ tests/route/test_file_location_database.cpp
@@ -1,19 +1,33 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "route/Location.h"
 #include "route/FileLocationDatabase.h"
 
 using namespace route;
 
+
+/// @brief this is the Mock File Database class, its used to mock and control the locations read from disk
 class MockFileLocationDatabase : public FileLocationDatabase {
 public:
     MockFileLocationDatabase(const std::string location_file) : FileLocationDatabase(location_file) {}
     
+    MOCK_METHOD(void, DeleteLocations, (std::vector<const Location* const>&), (override));
     MOCK_METHOD(std::vector<const Location* const>, GetLocationsOnDisk, (), (override));
-    
+
+    /// @brief Adapter method to call FileLocationDatabase::GetLocationsOnDisk from the mock
     std::vector<const Location* const> RealGetLocationsOnDisk() {
         return FileLocationDatabase::GetLocationsOnDisk();
     }
 
+    /// @brief Adapter method to call FileLocationDatabase::DeleteLocations from the mock
+    void RealDeleteLocations(std::vector<const Location* const>& locations) {
+        FileLocationDatabase::DeleteLocations(locations);
+    }
+
+    /// @brief Utility method to return a Location by name, useful for simplifying the test code
+    /// @param locations the list of locations to search
+    /// @param name The name of the location to find
+    /// @return The location on success, nullptr on failure
     const Location* const GetLocationByName(std::vector<const Location* const> locations, std::string name) {
         std::vector<const Location* const> results;
         std::for_each (locations.begin(), locations.end(), [this, &results, name](const Location* const location) mutable {
@@ -26,10 +40,27 @@ public:
             return results[0];
         }
         return nullptr;
-    }
-    
+    }  
 };
 
+/// @brief Test case to test MockFileLocationDatabase::GetLocationByName() which is used by the other tests
+TEST(AddTest, TestMockGetLocationByName)
+{
+    MockFileLocationDatabase test_db("mock_file.csv"); 
+
+    Location london("London", 5);
+    Location glasgow("Glasgow", 3);
+    Location brighton("Brighton", 1);
+
+    std::vector<const Location* const> locations = {&london, &glasgow, &brighton};
+
+    // Check finding each of the values, and they are the correct pointer
+    EXPECT_EQ(&london, test_db.GetLocationByName(locations, "London"));
+    EXPECT_EQ(&glasgow, test_db.GetLocationByName(locations, "Glasgow"));
+    EXPECT_EQ(&brighton, test_db.GetLocationByName(locations, "Brighton"));
+}
+
+/// @brief Test case for FileLocationDatabase::GetLocationsOnDisk() For testing successfully loading the locations from disk
 TEST(AddTest, TestLoadSuccess)
 {
     const std::string test_data_file = "test_data/route/test_load_success.csv";
@@ -59,20 +90,28 @@ TEST(AddTest, TestLoadSuccess)
     EXPECT_EQ(1, brighton->Cost());    
 }
 
+/// @brief Test case for FileLocationDatabase::GetLocationsOnDisk() For testing the locations source file not existing
 TEST(AddTest, TestLoadErrorFileMissing)
 {
     const std::string test_data_file = "test_data/route/test_non_existant_file.csv";
     MockFileLocationDatabase test_db(test_data_file);
+
+    EXPECT_CALL(test_db, DeleteLocations(testing::_)).Times(0);
 
     auto locations = test_db.RealGetLocationsOnDisk();
 
     EXPECT_EQ(0, locations.size());
 }
 
+/// @brief Test case for FileLocationDatabase::GetLocationsOnDisk() For testing a parsing error during loading the locations
 TEST(AddTest, TestLoadErrorWhileParsing)
 {
     const std::string test_data_file = "test_data/route/test_load_bad_data.csv";
     MockFileLocationDatabase test_db(test_data_file);
+
+    EXPECT_CALL(test_db, DeleteLocations(testing::_)).Times(1).WillOnce([&test_db](std::vector<const Location* const>& locations){
+        test_db.RealDeleteLocations(locations);
+    }); 
 
     auto locations = test_db.RealGetLocationsOnDisk();
 
